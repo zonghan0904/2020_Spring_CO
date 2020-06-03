b4060df08c362f876a62721fc6d19df1cea43184
zonghan <steven.me05@nctu.edu.tw>
2020-06-03 23:11:17 +0800

    implement backward search by means of FM-Index.

--- Part3/README.md
+++ Part3/README.md
@@ -1,21 +1,5 @@
 # 2020 CO-Final Project
 
-# How to download ?
-1. git clone https://github.com/Shalana/2020-CO-Final-Project.git
-
 # How to execute this program ?
-make sure your Cosmall.txt and FMIndex.cpp under the same folder.
-1. g++ FMIndex.cpp -o FMIndex.o
-2. ./FMIndex.o
-
-# How to generate new data ? 
-1. python generate.py | tee COsmall.txt
-
-#  Please make sure that your program can pass the following pattern.
-1. P1000(50%)、P3000(30%)、P10000(20%) 
-   * If you pass, you will get all the scores of correctness.
-   * TA will use P10000 to test your performance 
-
-
-
-If you have any confusion or suggestions, please let us know. Thanks.
+1. make
+2. ./Search
--- Part3/main.cpp
+++ Part3/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <string>
+#include <set>
 #include "FMIndex.h"
+
+#define BUFFERSIZE 1024
 
 using namespace std;
 
@@ -12,13 +16,28 @@ int main(int argc, char *argv[]){
     char ***suffixes_student=(char***)malloc(read_count*sizeof(char**));
     L_student=(char*)malloc(read_count*read_length*sizeof(char*));
 
+    string input;
+    cout << "please type input: ";
+    cin >> input;
+    int length = input.length();
+
+    int top = 0;
+    int bottom = read_count * read_length;
+    bool find = true;
+    int match_num = 0;
+    set<int> checked_data;
+    set<int>::iterator it;
+
+    int I_table[4] = {0, 0, 0, 0};
+    int **C_table = (int **)malloc((read_count * read_length + 1) * sizeof(int*));
+
     //-----------Time capture start--------------------
     struct timeval  TimeValue_Start;
     struct timeval  TimeValue_Final;
     struct timezone TimeZone_Start;
     struct timezone TimeZone_Final;
     long time_start, time_end;
-    double time_overhead_default, time_overhead_student;
+    double time_overhead_student;
 
     gettimeofday(&TimeValue_Start, &TimeZone_Start);
     time_start = TimeValue_Start.tv_sec * 1000000 + TimeValue_Start.tv_usec;
@@ -29,15 +48,71 @@ int main(int argc, char *argv[]){
 
     L_counts_student = makeFMIndex_student(suffixes_student, read_count, read_length, F_counts_student, L_student);
 
+    for (int i = 0; i < 4; i++){
+	if (i == 0) I_table[i] = F_counts_student[i] - 1;
+	else I_table[i] = I_table[i-1] + F_counts_student[i];
+    }
+    for (int i = 0; i < read_count * read_length + 1; i++){
+	C_table[i] = (int*)malloc(4*sizeof(int));
+	if (i == 0){
+	    memset(C_table[i], 0, 4 * sizeof(int));
+	}
+	else{
+	    C_table[i] = L_counts_student[i-1];
+	}
+    }
+
+    for (int i = length - 1; i >= 0; i--){
+	char c = input[i];
+	int index = 0;
+	switch (c){
+	    case 'A':
+		index = 0;
+		break;
+	    case 'C':
+		index = 1;
+		break;
+	    case 'G':
+		index = 2;
+		break;
+	    case 'T':
+		index = 3;
+		break;
+	    default:
+		break;
+	}
+
+	top = C_table[top][index] + I_table[index];
+	bottom = C_table[bottom][index] + I_table[index];
+
+	if (bottom <= top){
+	    find = false;
+	    match_num = 0;
+	    break;
+	}
+    }
+
+    if (find == true){
+	for (int i = top; i < bottom; i++){
+	     it = checked_data.find(SA_Final_student[i][1]);
+	     if (it != checked_data.end()){
+	         continue;
+	     }
+	     else{
+	         checked_data.insert(SA_Final_student[i][1]);
+	         match_num += 1;
+	     }
+	}
+    }
+
     gettimeofday(&TimeValue_Final, &TimeZone_Final);
     time_end = TimeValue_Final.tv_sec * 1000000 + TimeValue_Final.tv_usec;
     time_overhead_student = (time_end - time_start)/1000000.0;
 
-    //----------------For debug purpose only-----------------
-    //for(int i=0;i<read_count*read_length;i++)
-    //    cout<<L[i]<<"\t"<<SA_Final[i][0]<<","<<SA_Final[i][1]<<"\t"<<L_counts[i][0]<<","<<L_counts[i][1]<<","<<L_counts[i][2]<<","<<L_counts[i][3]<<endl;
-    //--------------------------------------------------
+    cout << "Number of reads match this substring: " << match_num << endl;
+    // for (it = checked_data.begin(); it != checked_data.end(); ++it)
+    //     cout << "found the substring in line: " << *it + 1 << endl;
 
-    cout <<  "student: " << time_overhead_student << endl;
+    // cout <<  "Execution time: " << time_overhead_student << endl;
     return 0;
 }
